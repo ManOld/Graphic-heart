6d8faf10092b4f176cdb1aacf1fe78a6b6beca65
Liuhao <540864802@qq.com>
2017-04-25 08:42:05 +0800

    Initial Commit

---
+++ hahah/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hahah
+//
+//  Created by admin on 2017/4/25.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
